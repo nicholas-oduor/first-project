6e488b4c3c771ab0af49e06125c83c51539a13df
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 13:57:03 +0300

    added class EditorTest

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Editor,Blog
 # Create your tests here.
+
+
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.nicholas= Editor(first_name = 'nicholas', last_name ='Oduor', email ='oduor5742@gmail.com')
