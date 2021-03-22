c6092774d4f5b492cbabb65818c97e8bceb7af09
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 13:52:48 +0300

    added public date

--- blogapp/models.py
+++ blogapp/models.py
@@ -20,3 +20,4 @@ class Blog(models.Model):
     post = models.TextField()
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
