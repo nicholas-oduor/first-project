5ca36919b984128b1aed97703efac8212ec4e46c
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 13:51:43 +0300

    added class Blog

--- blogapp/models.py
+++ blogapp/models.py
@@ -8,3 +8,15 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+class Blog(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
