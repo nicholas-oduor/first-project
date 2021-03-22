47559bc411a26c94191d41b6b9546b5cd96750c0
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 12:50:00 +0300

    added bootstrap

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import datetime as dt
 from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def blogs_of_day(request):
+    date = dt.date.today()
+    return render(request, 'today-blogs.html', {"date": date,})
+
+"""
+added function to present blogs from past days.
+"""
+
+def past_days_blogs(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(blogs_of_day)
+
+    return render(request, 'past-blogs.html', {"date": date})
