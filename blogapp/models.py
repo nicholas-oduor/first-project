796d5524170d5872c92c0d7d7ecb6369d81e0f80
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 13:34:28 +0300

    created class Editor

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+"""
+creating class editor and assigning models
+"""
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
