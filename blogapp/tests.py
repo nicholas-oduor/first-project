a1d8222e467785ae9c7fc954501c4c7afa478123
nicholas-oduor <oduor5742@gmail.com>
2021-03-22 14:15:59 +0300

    created super user successfully

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Editor,Blog
+from .models import Editor,Blog,tags
 # Create your tests here.
 
 
@@ -8,3 +8,6 @@ class EditorTestClass(TestCase):
     # Set up method
     def setUp(self):
         self.nicholas= Editor(first_name = 'nicholas', last_name ='Oduor', email ='oduor5742@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nicholas,Editor))
